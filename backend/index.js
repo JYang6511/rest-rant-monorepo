fbe8e30968bb2baed2d01c0b2089f130c46503a4
Johnson Yang <johnsonyang65@gmail.com>
2024-02-04 13:02:30 -0800

    add and delete comment work

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
-// Modules and Globals
+// Modules and Globals  
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
+const defineCurrentUser = require('./middleware/defineCurrentUser')
 
 // Express Settings
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(defineCurrentUser)
 
 // Controllers & Routes
 
@@ -23,5 +25,3 @@ app.use('/authentication', require('./controllers/authentication'))
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)
 })
-
-  
